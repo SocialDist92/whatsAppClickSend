cbe385332a119a41d2e44797fee0a2aab5b4888d
Armando <lrios@pkglobal.com>
2021-04-14 19:14:51 -0600

    open with whatsapp

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
+const { exec } = require("child_process");
+const open = require('open');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
@@ -89,6 +91,23 @@ function listLabels(auth) {
     });
 }
 
+function execTerminal(commandToDo) {
+    return new Promise((resolve, reject) => {
+        exec(commandToDo, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return reject(error.message);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return reject(stderr);
+            }
+            console.log(`stdout: ${stdout}`);
+            resolve(stdout)
+        });
+    })
+}
+
 
 /**
  * Lists the labels in the user's account.
@@ -106,16 +125,26 @@ function listEmail(auth) {
         gmail.users.messages.get({
             userId: 'me',
             id: res.data.messages[0].id
-        }).then((res) => {
-            console.log(res.data.payload.headers)
-            
-            let body_content = JSON.stringify(res.data.payload.parts[0].body.data);
+        }).then(async (res) => {
+            console.log(res.data.payload.headers);
+
+            /* let body_content = JSON.stringify(res.data.payload.parts[0].body.data);
             let data, buff, text;
             data = body_content;
             buff = new Buffer.from(data, "base64");
-            mailBody = buff.toString();
+            mailBody = buff.toString(); */
             // display the result
             //console.log(mailBody);
+
+            try {
+                await execTerminal('osascript openWhatsApp.scpt')
+                open('https://api.whatsapp.com/send/?phone=526143948254&text=Hola%2C+Aparte+un+boleto+de+la+rifa%21%21%0AMAZDA+CX-30+2021%21%21%0A%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%0A%5Cue110%2ABoleto%3A+%22+++number+++%22%2A+%28%22+++extraNumbers.join%28%27%2C+%27%29+++%22%29%0A%0A%2ANombre%3A%2A+%22+++formElements.name.value.toUpperCase%28%29+++%22+%22+++formElements.lastName.value.toUpperCase%28%29+++%22%0A%0A%5Cue125COSTO+BOLETO+%22+++price+++%22%0A%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%E2%80%94%0A%2ACLICK+AQU%C3%8D%3A%2A+lottosorteos.com%2Fpagos+Para+ver+cuentas+de+pago%21%0A%0AEl+siguiente+paso+es+enviar+foto+del+comprobante+de+pago+por+aqu%C3%AD&app_absent=0', {app: 'chrome'});
+
+            } catch (error) {
+                throw ('Failed to open WhatsApp');
+
+            }
+
         })
 
     });
